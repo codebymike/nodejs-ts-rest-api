888ca64a5467b67636ae91f51808123c1041bee2
Mike Thomas <mikethomas84@gmail.com>
2023-01-05 18:27:15 +0000

    basic user route

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -7,6 +7,15 @@ export class UsersRoutes extends CommonRoutesConfig {
     }
 
     configureRoutes() {
-        return this.app;
+
+        this.app.route(`/users`)
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).send(`List of users`)
+            })
+            .post((req: express.Request, res: express.Response) => {
+                res.status(200).send(`Post to users`)
+            })
+
+        return this.app
     }
 }
