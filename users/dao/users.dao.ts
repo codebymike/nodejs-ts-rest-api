ec9b7e8bcca91fbbc20db4ddc7d1150e2badfa09
Mike Thomas <mikethomas84@gmail.com>
2023-01-12 22:36:38 +0000

    read methods

--- users/dao/users.dao.ts
+++ users/dao/users.dao.ts
@@ -1,4 +1,4 @@
-import mongooseService from '../../common/services/mongoose.service';
+import mongooseService from '../../common/services/mongoose.service'
 
 import { CreateUserDto } from '../dto/create.user.dto'
 import { PatchUserDto } from '../dto/patch.user.dto'
@@ -11,7 +11,7 @@ const log: debug.IDebugger = debug('app:in-memory-dao')
 
 class UsersDao {
 
-    Schema = mongooseService.getMongoose().Schema;
+    Schema = mongooseService.getMongoose().Schema
 
     userSchema = new this.Schema({
         _id: String,
@@ -20,23 +20,38 @@ class UsersDao {
         firstName: String,
         lastName: String,
         permissionFlags: Number,
-    }, { id: false });
+    }, { id: false })
 
-    User = mongooseService.getMongoose().model('Users', this.userSchema);
+    User = mongooseService.getMongoose().model('Users', this.userSchema)
 
     constructor() {
         log('Created new instance of UsersDao')
     }
 
     async addUser(userFields: CreateUserDto) {
-        const userId = shortid.generate();
+        const userId = shortid.generate()
         const user = new this.User({
             _id: userId,
             ...userFields,
             permissionFlags: 1,
-        });
-        await user.save();
-        return userId;
+        })
+        await user.save()
+        return userId
+    }
+
+    async getUserByEmail(email: string) {
+        return this.User.findOne({ email: email }).exec()
+    }
+    
+    async getUserById(userId: string) {
+        return this.User.findOne({ _id: userId }).populate('User').exec()
+    }
+    
+    async getUsers(limit = 25, page = 0) {
+        return this.User.find()
+            .limit(limit)
+            .skip(limit * page)
+            .exec()
     }
 
 }
