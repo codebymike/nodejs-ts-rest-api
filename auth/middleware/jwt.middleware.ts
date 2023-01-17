89e4c56547581bbb583e9548073ef2108ae14804
Mike Thomas <mikethomas84@gmail.com>
2023-01-17 19:05:11 +0000

    jwt mw shell

--- auth/middleware/jwt.middleware.ts
+++ auth/middleware/jwt.middleware.ts
@@ -3,4 +3,14 @@ import jwt from 'jsonwebtoken'
 import crypto from 'crypto'
 
 import { Jwt } from '../../common/types/jwt'
-import usersService from '../../users/services/users.service';
+import usersService from '../../users/services/users.service'
+
+
+// @ts-expect-error
+const jwtSecret: string = process.env.JWT_SECRET
+
+
+class JwtMiddleware {
+}
+
+export default new JwtMiddleware()
