6420b7a237c986d22f70ba14937f8642d5c15b21
Mike Thomas <mikethomas84@gmail.com>
2023-01-17 19:05:41 +0000

    jwt mw refresh

--- auth/middleware/jwt.middleware.ts
+++ auth/middleware/jwt.middleware.ts
@@ -11,6 +11,20 @@ const jwtSecret: string = process.env.JWT_SECRET
 
 
 class JwtMiddleware {
+
+    verifyRefreshBodyField(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        if (req.body && req.body.refreshToken) {
+            return next()
+        } else {
+            return res
+                .status(400)
+                .send({ errors: ['Missing required field: refreshToken'] })
+        }
+    }
 }
 
 export default new JwtMiddleware()
