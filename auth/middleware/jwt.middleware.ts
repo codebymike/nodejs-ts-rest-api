52e751749cddcc746fe07993cb8c260e4e992da5
Mike Thomas <mikethomas84@gmail.com>
2023-01-17 19:06:23 +0000

    jwt mw valid needed

--- auth/middleware/jwt.middleware.ts
+++ auth/middleware/jwt.middleware.ts
@@ -52,6 +52,31 @@ class JwtMiddleware {
             return res.status(400).send({ errors: ['Invalid refresh token'] })
         }
     }
+
+    validJWTNeeded(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        if (req.headers['authorization']) {
+            try {
+                const authorization = req.headers['authorization'].split(' ')
+                if (authorization[0] !== 'Bearer') {
+                    return res.status(401).send()
+                } else {
+                    res.locals.jwt = jwt.verify(
+                        authorization[1],
+                        jwtSecret
+                    ) as Jwt
+                    next()
+                }
+            } catch (err) {
+                return res.status(403).send()
+            }
+        } else {
+            return res.status(401).send()
+        }
+    }
 }
 
 export default new JwtMiddleware()
