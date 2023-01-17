2e2f8d5aeb009ac8671b52002b25402b63e077a7
Mike Thomas <mikethomas84@gmail.com>
2023-01-17 19:06:02 +0000

    jwt mw refresh needed

--- auth/middleware/jwt.middleware.ts
+++ auth/middleware/jwt.middleware.ts
@@ -25,6 +25,33 @@ class JwtMiddleware {
                 .send({ errors: ['Missing required field: refreshToken'] })
         }
     }
+
+    async validRefreshNeeded(
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        const user: any = await usersService.getUserByEmailWithPassword(
+            res.locals.jwt.email
+        )
+        const salt = crypto.createSecretKey(
+            Buffer.from(res.locals.jwt.refreshKey.data)
+        )
+        const hash = crypto
+            .createHmac('sha512', salt)
+            .update(res.locals.jwt.userId + jwtSecret)
+            .digest('base64')
+        if (hash === req.body.refreshToken) {
+            req.body = {
+                userId: user._id,
+                email: user.email,
+                permissionFlags: user.permissionFlags,
+            }
+            return next()
+        } else {
+            return res.status(400).send({ errors: ['Invalid refresh token'] })
+        }
+    }
 }
 
 export default new JwtMiddleware()
