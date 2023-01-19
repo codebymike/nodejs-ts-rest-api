7176e8b38951a91f965899085cc98c7fb22778c0
Mike Thomas <mikethomas84@gmail.com>
2023-01-19 23:49:12 +0000

    perm factory

--- common/middleware/common.permission.middleware.ts
+++ common/middleware/common.permission.middleware.ts
@@ -3,3 +3,29 @@ import { PermissionFlag } from './common.permissionflag.enum'
 import debug from 'debug'
 
 const log: debug.IDebugger = debug('app:common-permission-middleware')
+
+class CommonPermissionMiddleware {
+
+    permissionFlagRequired(requiredPermissionFlag: PermissionFlag) {
+        return (
+            req: express.Request,
+            res: express.Response,
+            next: express.NextFunction
+        ) => {
+            try {
+                const userPermissionFlags = parseInt(
+                    res.locals.jwt.permissionFlags
+                )
+                if (userPermissionFlags & requiredPermissionFlag) {
+                    next()
+                } else {
+                    res.status(403).send()
+                }
+            } catch (e) {
+                log(e)
+            }
+        }
+    }
+}
+
+export default new CommonPermissionMiddleware()
